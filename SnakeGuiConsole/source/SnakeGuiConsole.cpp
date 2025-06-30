7154d01b393c9204ac578b09848475bdb0139f55
Majky <michal.derer@gmail.com>
2025-06-30 14:15:22 +0200

    c

--- SnakeGuiConsole/source/SnakeGuiConsole.cpp
+++ SnakeGuiConsole/source/SnakeGuiConsole.cpp
@@ -1,277 +1,198 @@
-﻿#include <stdio.h>
+﻿//#include <stdio.h>
+//#include <iostream>
+//#include <string>
+//#include <windows.h>
+//
+//#include <fcntl.h>
+//#include <io.h>
+//
+//#include <SnakeLib.hpp>
+
+
+
+//void ClearConsole()
+//{
+//    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+//    if (hConsole == INVALID_HANDLE_VALUE) return;
+//
+//    CONSOLE_SCREEN_BUFFER_INFO csbi;
+//    DWORD cellCount, count;
+//    COORD homeCoords = { 0, 0 };
+//
+//    if (!GetConsoleScreenBufferInfo(hConsole, &csbi)) return;
+//
+//    cellCount = csbi.dwSize.X * csbi.dwSize.Y;
+//
+//    // Fill the screen with blanks
+//    FillConsoleOutputCharacter(hConsole, ' ', cellCount, homeCoords, &count);
+//    FillConsoleOutputAttribute(hConsole, csbi.wAttributes, cellCount, homeCoords, &count);
+//
+//    // Move the cursor to the top-left corner
+//    SetConsoleCursorPosition(hConsole, homeCoords);
+//}
+
+
+//int main() 
+//{
+//    
+//    
+//    SnakeGame game{Dimensions{2,2}};
+//
+//
+//
+//    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+//    HANDLE hInput = GetStdHandle(STD_INPUT_HANDLE);
+//
+//    if (hConsole == INVALID_HANDLE_VALUE) return -1;
+//    if (hInput == INVALID_HANDLE_VALUE) return -2;
+//
+//    CONSOLE_SCREEN_BUFFER_INFO csbi;
+//    DWORD cellCount, count;
+//    COORD homeCoords = { 0, 0 };
+//
+//    DWORD events = 0;
+//    INPUT_RECORD inputRecord;
+//
+//
+//    //std::cout << "This will be cleared in 3 seconds..." << std::endl;
+//    //Sleep(3000); // Pause for 3 seconds
+//    //ClearConsole();
+//    //std::cout << "Console cleared!" << std::endl;
+//
+//    //setup_console_utf8();
+//    ClearConsole();
+//    draw();
+//
+//    // Enable window input
+//    SetConsoleMode(hInput, ENABLE_WINDOW_INPUT | ENABLE_PROCESSED_INPUT);
+//    //std::cout << "Press ESC to quit...\n";
+//
+//    //int i = 0;
+//    while (true) 
+//    {
+//        //ClearConsole();
+//        //draw();
+//        //Sleep(1000);
+//
+//        //std::cout << game.random(10) << "\n";
+//
+//        int x[10] = {};
+//        for (int i = 0; i < 999; i++)
+//        {
+//            x[game.random(10)] += 1;
+//        }
+//
+//        for (int i = 0; i < 10; i++)
+//            std::cout << x[i] << "\n";
+//
+//    //    draw();
+//    //    std::cout << "i:" << ++i << "\n";
+//    //    Sleep(1000);
+//
+//
+//        //ReadConsoleInput(hInput, &inputRecord, 1, &events);
+//
+//        //if (inputRecord.EventType == KEY_EVENT && inputRecord.Event.KeyEvent.bKeyDown) {
+//        //    char ch = inputRecord.Event.KeyEvent.uChar.AsciiChar;
+//        //    int vk = inputRecord.Event.KeyEvent.wVirtualKeyCode;
+//
+//        //    //std::cout << "Key pressed: " << ch << " (VK: " << vk << ")\n";
+//
+//        //    //if (ch == 'c') ClearConsole();
+//
+//        //    if (vk == VK_ESCAPE) break;
+//        //}
+//    }
+//
+//    return 0;
+//}
+
+
+
+
+
 #include <iostream>
-#include <string>
+#include <conio.h>
 #include <windows.h>
 
+//redraw
 
-#include <fcntl.h>
-#include <io.h>
-
-
-#include <SnakeLib.hpp>
-
-
-
-void setup_console_utf8() 
-{
-    // Set the console to UTF-8
-    SetConsoleOutputCP(CP_UTF8);
-    SetConsoleCP(CP_UTF8);
-
-    // Optional: set output mode for wide text if using std::wcout
-    //_setmode(_fileno(stdout), _O_U8TEXT);
-}
-
-void ClearConsole()
-{
-    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-    if (hConsole == INVALID_HANDLE_VALUE) return;
-
-    CONSOLE_SCREEN_BUFFER_INFO csbi;
-    DWORD cellCount, count;
-    COORD homeCoords = { 0, 0 };
-
-    if (!GetConsoleScreenBufferInfo(hConsole, &csbi)) return;
-
-    cellCount = csbi.dwSize.X * csbi.dwSize.Y;
-
-    // Fill the screen with blanks
-    FillConsoleOutputCharacter(hConsole, ' ', cellCount, homeCoords, &count);
-    FillConsoleOutputAttribute(hConsole, csbi.wAttributes, cellCount, homeCoords, &count);
-
-    // Move the cursor to the top-left corner
-    SetConsoleCursorPosition(hConsole, homeCoords);
-}
-
-
-std::string make_line()
-{
-    int l = 5;
-    int symbol = std::rand() % l;
-    std::string s{};
-
-    s.append("|");
-    for (int i = 0; i < l; i++)
-    {
-        s.append((i == symbol ? "A" : "0"));
-    }
-    s.append("|");
-    return s;
-}
-
-void draw()
-{
-    //std::string s = make_table();
-
-    /*std::u8string s = u8"╔═╗";
-    std::string s_utf8(s.begin(), s.end());
-
-    std::cout << s_utf8 << std::endl;*/
-
-    std::cout << make_line() << "\n";
-}
+//void setCursorPosition(int x, int y) {
+//    COORD coord = { (SHORT)x, (SHORT)y };
+//    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+//}
+//
+//int main() {
+//    int playerX = 5;
+//    bool running = true;
+//
+//    // Hide the blinking cursor
+//    HANDLE out = GetStdHandle(STD_OUTPUT_HANDLE);
+//    CONSOLE_CURSOR_INFO cursorInfo;
+//    GetConsoleCursorInfo(out, &cursorInfo);
+//    cursorInfo.bVisible = false;
+//    SetConsoleCursorInfo(out, &cursorInfo);
+//
+//    while (running) {
+//        setCursorPosition(0, 0);  // Reset to top-left instead of clearing screen
+//
+//        // Draw scene
+//        for (int i = 0; i < playerX; ++i) std::cout << ' ';
+//        std::cout << "@       \n";  // Overdraw with padding
+//        std::cout << "Use A/D to move. Press Q to quit.\n";
+//
+//        // Handle input
+//        if (_kbhit()) {
+//            char ch = _getch();
+//            ch = tolower(ch);
+//
+//            if (ch == 'a' && playerX > 0) playerX--;
+//            else if (ch == 'd') playerX++;
+//            else if (ch == 'q') running = false;
+//        }
+//
+//        Sleep(50);
+//    }
+//
+//    // Restore cursor
+//    cursorInfo.bVisible = true;
+//    SetConsoleCursorInfo(out, &cursorInfo);
+//
+//    return 0;
+//}
 
 
+//input
 
-
-int main() 
-{
-    
-    
-    SnakeGame game{Dimensions{2,2}};
-
-
-
-    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+int main() {
     HANDLE hInput = GetStdHandle(STD_INPUT_HANDLE);
+    DWORD mode;
+    GetConsoleMode(hInput, &mode);
+    SetConsoleMode(hInput, mode & ~(ENABLE_LINE_INPUT | ENABLE_ECHO_INPUT)); // raw input mode
 
-    if (hConsole == INVALID_HANDLE_VALUE) return -1;
-    if (hInput == INVALID_HANDLE_VALUE) return -2;
-
-    CONSOLE_SCREEN_BUFFER_INFO csbi;
-    DWORD cellCount, count;
-    COORD homeCoords = { 0, 0 };
-
-    DWORD events = 0;
     INPUT_RECORD inputRecord;
+    DWORD events;
+    bool running = true;
 
+    std::cout << "Press arrow keys or Q to quit.\n";
 
-    //std::cout << "This will be cleared in 3 seconds..." << std::endl;
-    //Sleep(3000); // Pause for 3 seconds
-    //ClearConsole();
-    //std::cout << "Console cleared!" << std::endl;
+    while (running) {
+        ReadConsoleInput(hInput, &inputRecord, 1, &events);
 
-    //setup_console_utf8();
-    ClearConsole();
-    draw();
-
-    // Enable window input
-    SetConsoleMode(hInput, ENABLE_WINDOW_INPUT | ENABLE_PROCESSED_INPUT);
-    //std::cout << "Press ESC to quit...\n";
-
-    //int i = 0;
-    while (true) 
-    {
-        //ClearConsole();
-        //draw();
-        //Sleep(1000);
-
-        //std::cout << game.random(10) << "\n";
-
-        int x[10] = {};
-        for (int i = 0; i < 999; i++)
-        {
-            x[game.random(10)] += 1;
+        if (inputRecord.EventType == KEY_EVENT && inputRecord.Event.KeyEvent.bKeyDown) {
+            switch (inputRecord.Event.KeyEvent.wVirtualKeyCode) {
+            case VK_LEFT:
+                std::cout << "LEFT\n";
+                break;
+            case VK_RIGHT:
+                std::cout << "RIGHT\n";
+                break;
+            case 'Q':
+                running = false;
+                break;
+            }
         }
-
-        for (int i = 0; i < 10; i++)
-            std::cout << x[i] << "\n";
-
-    //    draw();
-    //    std::cout << "i:" << ++i << "\n";
-    //    Sleep(1000);
-
-
-        //ReadConsoleInput(hInput, &inputRecord, 1, &events);
-
-        //if (inputRecord.EventType == KEY_EVENT && inputRecord.Event.KeyEvent.bKeyDown) {
-        //    char ch = inputRecord.Event.KeyEvent.uChar.AsciiChar;
-        //    int vk = inputRecord.Event.KeyEvent.wVirtualKeyCode;
-
-        //    //std::cout << "Key pressed: " << ch << " (VK: " << vk << ")\n";
-
-        //    //if (ch == 'c') ClearConsole();
-
-        //    if (vk == VK_ESCAPE) break;
-        //}
     }
 
     return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//#include <iostream>
-////#include "snake.hpp"
-//
-//
-//int main()
-//{
-//	try
-//	{
-//		/*Dimensions d1{ 1,1 };
-//		SnakeGame g1(d1);
-//
-//		SnakeGame g2(Dimensions{2,2});
-//
-//		auto dd1 = g1.get_dimensions();
-//		auto dd2 = g2.get_dimensions();
-//
-//		std::cout << "x:" << dd1.x_size << " y:" << dd1.y_size << std::endl;
-//		std::cout << "x:" << dd2.x_size << " y:" << dd2.y_size << std::endl;*/
-//
-//		;
-//
-//		SnakeGame g1(Dimensions{ 2,2 });
-//
-//		auto dd1 = g1.get_dimensions();
-//
-//		std::cout << "x:" << dd1.x_size << " y:" << dd1.y_size << std::endl;
-//
-//
-//	}
-//	catch (const std::exception& e)
-//	{
-//		std::cout << e.what() << std::endl;
-//	}
-//
-//	return 0;
-//}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//------------------------------------------------------------------------
-
-
-//#include <iostream>
-//#include "SnakeGuiConsole.hpp"
-//#include "SnakeLib.hpp"
-//
-//using namespace std;
-//
-//int main()
-//{ 
-//	cout << "Snake CONSOLE." << endl;
-//
-//	TestLib lib{};
-//
-//	cout << endl;
-//	lib.message();
-//	cout << endl;
-//
-//	const ITestDataPublic***& data = lib.get_data();
-//
-//	((TestData*)data[0][0])->x = 1;
-//
-//	for (int i = 0; i < 2; i++)
-//		for (int j = 0; j < 2; j++)
-//		{
-//			cout << "Data at i:" << i << " j:" << j << " = " 
-//				<< data[i][j]->get_x() 
-//				<< ", " 
-//				<< data[i][j]->get_y() 
-//				<< endl;
-//
-//			//cout << "Data at i:" << i << " j:" << j << " = " 
-//			//	<< lib.get_data(i,j).get_x()
-//			//	<< ", " 
-//			//	<< lib.get_data(i, j).get_y()
-//			//	<< endl;
-//		}
-//
-//	cout << endl;
-//
-//	const int** start = lib.get_xarr();
-//	
-//	int x[2] = {2,2};
-//	start[0] = x;
-//	start[1] = x;
-//
-//	for (int i = 0; i < 2; i++)
-//		for (int j = 0; j < 2; j++)
-//			cout << " " << start[i][j] << " ";
-//
-//
-//	cout << endl;
-//	return 0;
-//}
